1ce1f3e5a419ba3ca12ac8a8aaf604b9b2622898
Noor <noorpatel1290@gmail.com>
2025-03-13 22:03:20 +0400

    Added Day 7 Meeting Minutes

--- VideoRentalSystem/VideoRentalSystem/obj/Debug/net8.0-windows/VideoRentalSystem.AssemblyInfo.cs
+++ VideoRentalSystem/VideoRentalSystem/obj/Debug/net8.0-windows/VideoRentalSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoRentalSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66ad5439ffcdf7bcbdb56abe244d1103b64565fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b96344cab9ce180fd3e8849d0b1dbe810415f09b")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoRentalSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoRentalSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- VideoRentalSystem/VideoRentalSystem/obj/Release/net8.0-windows/VideoRentalSystem.AssemblyInfo.cs
+++ VideoRentalSystem/VideoRentalSystem/obj/Release/net8.0-windows/VideoRentalSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoRentalSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de919157b7934b286e3f0a5406670d48640c942a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b96344cab9ce180fd3e8849d0b1dbe810415f09b")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoRentalSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoRentalSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
