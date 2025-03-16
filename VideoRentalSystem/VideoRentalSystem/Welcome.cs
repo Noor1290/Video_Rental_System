fbf71be825e4f3c6fd8542596536a38a2b221a6a
Noor <noorpatel1290@gmail.com>
2025-03-16 18:50:12 +0400

    Revert "Chnaged in the UI Design"
    This reverts commit 3eef14360030c16713131d77283f9fbf795d225f, reversing
    changes made to 4f75026ebb84c9feb709490904895c1c10c104d2.

--- VideoRentalSystem/VideoRentalSystem/Welcome.cs
+++ VideoRentalSystem/VideoRentalSystem/Welcome.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
-using Last;
 using Timer = System.Windows.Forms.Timer;
 
 
 namespace VideoRentalSystem
 {
-    [System.Runtime.Versioning.SupportedOSPlatform("windows")]
     // Main form class for the welcome screen
     public partial class WelcomeForm : Form
     {
@@ -36,21 +34,18 @@ namespace VideoRentalSystem
         {
             // Form properties
             this.Text = "Welcome Window";
-            this.AutoScaleDimensions = new SizeF(9F, 20F);
-            this.AutoScaleMode = AutoScaleMode.Font;
-            this.ClientSize = new Size(875, 650);
+            this.Size = new Size(900, 500);
             this.StartPosition = FormStartPosition.CenterScreen;
-            this.BackColor = Color.LightBlue; // LightBlue background
+            this.BackColor = Color.FromArgb(0, 31, 63); // Dark blue background
 
             // Welcome Label
             lblWelcome = new Label
             {
                 Text = "", // Starts empty for typing effect
-                ForeColor = Color.Black,
-                Font = new Font("Lucida Handwriting", 18F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0))),
+                ForeColor = Color.White,
+                Font = new Font("Arial", 24, FontStyle.Bold),
                 AutoSize = true,
-                Location = new Point(125, 45), // Position at the top center
-                TextAlign = ContentAlignment.MiddleCenter,
+                Location = new Point(200, 50) // Position at the top center
             };
             this.Controls.Add(lblWelcome);
 
@@ -58,12 +53,11 @@ namespace VideoRentalSystem
             btnLogin = new Button
             {
                 Text = "Login",
-                Size = new Size(175, 90),
-                Location = new Point(360, 230),
-                BackColor = SystemColors.ActiveCaption,
+                Size = new Size(120, 60),
+                Location = new Point(340, 200),
+                BackColor = Color.Blue,
                 ForeColor = Color.White,
-                Font = new Font("Lucida Handwriting", 15F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0))),
-                Anchor = AnchorStyles.Bottom,
+                Font = new Font("Arial", 14, FontStyle.Bold)
             };
             btnLogin.Click += BtnLogin_Click; // Attach click event
             this.Controls.Add(btnLogin);
@@ -72,14 +66,12 @@ namespace VideoRentalSystem
             btnRegister = new Button
             {
                 Text = "Register",
-                Size = new Size(175, 90),
-                Location = new Point(360, 350),
-                BackColor = SystemColors.ActiveCaption,
+                Size = new Size(120, 60),
+                Location = new Point(340, 280),
+                BackColor = Color.Blue,
                 ForeColor = Color.White,
-                Font = new Font("Lucida Handwriting", 15F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0))),
-                Anchor = AnchorStyles.Top,
+                Font = new Font("Arial", 14, FontStyle.Bold)
             };
-        
             btnRegister.Click += BtnRegister_Click; // Attach click event
             this.Controls.Add(btnRegister);
         }
@@ -111,7 +103,7 @@ namespace VideoRentalSystem
             {
                 if (lblWelcome.ForeColor.A > 20) // Ensure Alpha value doesn't go negative
                 {
-                    lblWelcome.ForeColor = Color.FromArgb(lblWelcome.ForeColor.A - 20, 255, 255);
+                    lblWelcome.ForeColor = Color.FromArgb(lblWelcome.ForeColor.A - 20, 255, 255, 255);
                 }
                 else
                 {
@@ -129,18 +121,13 @@ namespace VideoRentalSystem
         // Event handler for Login button click
         private void BtnLogin_Click(object sender, EventArgs e)
         {
-            Login login = new Login();
-            login.Show();
-            this.Hide();
+            
         }
 
         // Event handler for Register button click
         private void BtnRegister_Click(object sender, EventArgs e)
         {
-            Register register = new Register();
-            register.Show();
-            this.Hide();
-
+            
         }
     }
 }
