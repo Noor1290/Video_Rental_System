5b8368f9ef8a88343270db97d25138230aecd8a0
Noor <noorpatel1290@gmail.com>
2025-03-29 23:09:18 +0400

    Added Day 23 Meeting Minutes

--- VideoRentalSystem/Testing/obj/Debug/net8.0-windows7.0/Testing.AssemblyInfo.cs
+++ VideoRentalSystem/Testing/obj/Debug/net8.0-windows7.0/Testing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c3df81c9c0eb90c16bb542f22d19d84ce8003c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d378c7cfde3d1900e1ac24fe3f0ca923890e167")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
